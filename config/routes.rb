03bb1c8be01508ae3046797659693471ff248077
EC2 Default User <ec2-user@ip-172-31-36-173.ec2.internal>
2021-02-16 02:43:24 +0000

    admin ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,7 @@
 Rails.application.routes.draw do
-  get 'admin_genres/index'
-
-  get 'admin_genres/create'
-
-  get 'admin_genres/edit'
-
-  get 'admin_genres/update'
-
-  get 'admin_items/index'
-
-  get 'admin_items/new'
-
-  get 'admin_items/create'
-
-  get 'admin_items/show'
-
-  get 'admin_items/edit'
-
-  get 'admin_items/update'
-
-  get 'admin_orders/index'
-
-  get 'admin_orders/show'
-
-  devise_for :users
+  devise_for :admins
+  devise_for :customers
+  
   get 'top' => 'homes#top'
   get 'about' => 'homes#about'
 
@@ -37,4 +15,10 @@ Rails.application.routes.draw do
   resources :cart_items, only: [:index, :create, :update, :destroy]
   delete 'destroy_all' => 'cart_items#destroy_all'
   resources :shippings, only: [:index, :create, :edit, :update, :destroy]
+  
+  resources :admin_customers, only: [:index, :show, :edit, :update]
+  resources :admin_items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :admin_orders, only: [:show, :index]
+  patch 'update' => 'admin_order_details#update'
+  resources :admin_genres, only: [:index, :create, :edit, :update]
 end
