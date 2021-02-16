4c06efa34120c73c75de5f117ed814b0394c6d18
EC2 Default User <ec2-user@ip-172-31-10-76.ec2.internal>
2021-02-16 05:11:40 +0000

    tabl小島分追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210214055456) do
+ActiveRecord::Schema.define(version: 20210216030204) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer  "customer_id", null: false
+    t.integer  "item_id",     null: false
+    t.integer  "amount",      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "genre_id",                   null: false
+    t.string   "name",                       null: false
+    t.text     "description",                null: false
+    t.integer  "price",                      null: false
+    t.string   "image_id",                   null: false
+    t.boolean  "is_active",   default: true, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
