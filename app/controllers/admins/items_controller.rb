2408a24c7aae8281283c5f852092944fa9bb2b96
EC2 Default User <ec2-user@ip-172-31-10-76.ec2.internal>
2021-02-17 11:12:14 +0000

    adimsの新規登録画面

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,11 +1,18 @@
 class Admins::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    # @item.user_id = current_user.id
+    # byebug
+    @item.save
+    redirect_to admins_items_path
   end
 
   def show
@@ -15,5 +22,10 @@ class Admins::ItemsController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :genre_id, :image, :is_active, :created_at, :updated_at)
   end
 end
