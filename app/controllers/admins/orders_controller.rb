f804636ae50ab5cf77b5ae9ebb46fd7ac315b01e
EC2 Default User <ec2-user@ip-172-31-36-173.ec2.internal>
2021-02-18 03:19:25 +0000

    add 注文詳細

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,13 +1,24 @@
 class Admins::OrdersController < ApplicationController
   def index
-     @order = Order.all.page(params[:page]).per(10)
+     @orders = Order.all.page(params[:page]).per(10)
+     @customer = Customer.find(params[:id])
   end
 
   def show
+     @order = Order.find(params[:id])
+     @customer = Customer.find(params[:id])
+     @items = Item.all
+     @order_details = @order.order_details
   end
   
-   private
+  private
   def order_prams
     params.require(:order).permit(:postal_code, :address, :address_name, :postage, :payment, :order_status, :total_payment, :created_at)
   end  
+  def customer_prams
+    params.require(:customer).permit(:last_name, :first_name, :email, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :is_deleted)
+  end
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :genre_id, :image, :is_active, :created_at, :updated_at)
+  end
 end
