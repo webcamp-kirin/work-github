ae7d94ac40c41756e56a09c4d92c6a17d1d9235e
EC2 Default User <ec2-user@ip-172-31-36-173.ec2.internal>
2021-02-18 11:15:53 +0000

    add 注文詳細

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,14 @@
 class Admins::OrdersController < ApplicationController
   def index
      @orders = Order.all.page(params[:page]).per(10)
-     @customer = Customer.find(params[:id])
+     if params[:id]
+       @customer = Customer.find(params[:id])
+       @order = @customer.orders
+       ## customer
+     else
+       @order = Order.all## order
+     end
+    
   end
 
   def show
@@ -12,10 +19,10 @@ class Admins::OrdersController < ApplicationController
   end
   
   private
-  def order_prams
+  def order_params
     params.require(:order).permit(:postal_code, :address, :address_name, :postage, :payment, :order_status, :total_payment, :created_at)
   end  
-  def customer_prams
+  def customer_params
     params.require(:customer).permit(:last_name, :first_name, :email, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :is_deleted)
   end
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,5 @@ class Item < ApplicationRecord
  attachment :image
  belongs_to :genre
  has_many :cart_items, dependent: :destroy
- has_many :orders, though: :order_details
  has_many :order_details
 end
